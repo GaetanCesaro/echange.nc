dd4706d7cc6f5f4395eb71b08156158606ef37eb
Gaëtan CESARO <gaetan.cesaro@gmail.com>
2019-07-20 16:03:37 +1100

    Passage en SCSS et theme bootstrap build

--- src/app/home/service-details/service-details.component.ts
+++ src/app/home/service-details/service-details.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'service-details',
   templateUrl: './service-details.component.html',
-  styleUrls: ['./service-details.component.css']
+  styleUrls: ['./service-details.component.scss']
 })
 export class ServiceDetailsComponent implements OnInit {
 
