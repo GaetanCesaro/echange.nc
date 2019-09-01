c7595abe45d0d917953e0fd155f5c09b14013522
Gaëtan CESARO <gaetan.cesaro@gmail.com>
2019-09-01 16:08:06 +1100

    chore: Optimize sample images

--- src/app/home/ad/ad.service.ts
+++ src/app/home/ad/ad.service.ts
@@ -18,36 +18,36 @@ const ads : Ad[] = [
     id: 1,
     title: "Bricolage mobilier intérieur",
     description: "Tous bricolages intérieurs, montage de rideaux, fixations murales, petites réparation de meubles, etc...",
-    image: "bricolage.jpeg"
+    image: "bricolage.jpg"
   },
   {
     id: 2,
     title: "Plomberie",
     description: "Plomberie intérieure (réparations, installations, etc...)",
-    image: "plombier.jpeg"
+    image: "plombier.jpg"
   },
   {
     id: 3,
     title: "Electricité générale",
     description: "Opérations d'élécticité d'usage général intérieur et extèrieur",
-    image: "electricien.jpeg"
+    image: "electricien.jpg"
   },
   {
     id: 4,
     title: "Installations d'éviers",
     description: "Eviers, mitigeurs, douches, etc...",
-    image: "evier.jpeg"
+    image: "evier.jpg"
   },
   {
     id: 5,
     title: "Petit outillage",
     description: "Tous types d'outils portatifs (tournevis, clés, visseuses, etc...)",
-    image: "outils.jpeg"
+    image: "outils.jpg"
   },
   {
     id: 6,
     title: "Outils de découpe",
     description: "Outillage massif de découpe (scies, scies circulaires, foreuses, etc...)",
-    image: "scie_circulaire.jpeg"
+    image: "scie_circulaire.jpg"
   }
 ]
