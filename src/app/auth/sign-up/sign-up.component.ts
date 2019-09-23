1b5a0ac17e374265020f9f0d3a64bca5e5e65c27
Gaetan CESARO <gcesaro@ncit.nc>
2019-09-23 16:34:34 +1100

    feat: Trying to add User to DB...

--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -1,14 +1,13 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import * as firebase from 'firebase';
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import * as firebase from "firebase";
 
 @Component({
-  selector: 'sign-up',
-  templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+  selector: "sign-up",
+  templateUrl: "./sign-up.component.html",
+  styleUrls: ["./sign-up.component.scss"]
 })
 export class SignUpComponent {
-
   email: string;
   password: string;
   passwordBis: string;
@@ -16,21 +15,50 @@ export class SignUpComponent {
   signUpInvalid = false;
   errorMessage: string;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
-  login(formValues) {
-    firebase.auth().createUserWithEmailAndPassword(formValues.email, formValues.password).catch(function(error) {
-      console.log("ERROR - Firebase createUserWithEmailAndPassword() - " + error)
+  signup(formValues) {
+    var email = formValues.email;
 
-      this.errorMessage = error.message;
-      this.signUpInvalid = true;
-    }).finally(function(){
-      this.router.navigate(["/home"]);
-    });
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(formValues.email, formValues.password)
+      .catch(function(error) {
+        console.log(
+          "ERROR - Firebase createUserWithEmailAndPassword() - " + error
+        );
+
+        this.errorMessage = error.message;
+        this.signUpInvalid = true;
+      })
+      .then(function success() {
+        return this.addUser(email);
+      })
+      .finally(function() {
+        this.router.navigate(["/home"]);
+      });
   }
 
   cancel() {
     this.router.navigate(["/home"]);
   }
 
+  addUser(email) {
+    var db = firebase.firestore();
+
+    console.log({ email });
+
+    db.collection("users")
+      .add({
+        firstname: "Jean",
+        lastname: "Louis",
+        email: email
+      })
+      .then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+      })
+      .catch(function(error) {
+        console.error("Error adding document: ", error);
+      });
+  }
 }
